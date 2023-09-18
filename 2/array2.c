e9e829307e0d0cd42d736fd1513595883f2a254b
c76258yx <yongyan.xie@student.manchester.ac.uk>
2023-09-18 10:56:17 +0100

    a

--- 2/array2.c
+++ 2/array2.c
@@ -4,14 +4,14 @@
 
 int main(int argc, char **argv) {
 
-    int a[argc - 1];
+    int array[argc - 1];
 
     for (int i = 1; i < argc; i++) {
-        a[i - 1] = atoi(argv[i]);
+        array[i - 1] = atoi(argv[i]);
     }
 
     for (int i = 0; i < argc - 1; i++) {
-        if (a[i] % 2 == 0) {
+        if (array[i] % 2 == 0) {
             printf("%d is even", a[i]);
         } else {
             printf("%d is odd", a[i]);
