1c8431126a226afe5addc43c33e0cdeea656cb4f
c76258yx <yongyan.xie@student.manchester.ac.uk>
2023-09-28 11:16:48 +0100

    a

--- 3/test.c
+++ 3/test.c
@@ -1,24 +1,10 @@
 
-
 #include <stdio.h>
-#include <stdlib.h>
 
-void print_ten_integers() {
-    int *array = malloc(10 * sizeof(int));
-    if(!array) {
-        printf("cannot allocate memory ...\n");
-        return;
-    }
-    for(int i=0; i<10; i++) {
-        array[i] = rand()%100;
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-    /* array is neve freed, leaking 10*sizeof(int) of memory each iteration */
-}
-int main(int argc, char **argv) {
-    int iterations = atoi(argv[1]);
-    for(int i=0; i<iterations; i++)
-        print_ten_integers();
+int main() {
+    char *a = "Hello World";
+    a[sizeof(a) - 2] = 'a'; 
+    printf("%s\n", a);  
     return 0;
 }
+
