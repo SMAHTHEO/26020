b2a953911364bf9e42712667cbdcaa3bf4a9d5fe
c76258yx <yongyan.xie@student.manchester.ac.uk>
2023-09-28 21:48:50 +0100

    a

--- 3/mollac4.c
+++ 3/mollac4.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     // 填数
     for(int i=0; i<10; i++) {
         a[i] = i;
-        printf("%d ", a[i]);
+        printf("%d\n", a[i]);
     }
 
     free(a);
---
+++ 3/mollac5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *my_realloc(void *ptr, size_t old_size, size_t new_size) {
+    // 如果 ptr 为空，那么就直接 malloc
+    if(!ptr) return malloc(new_size);
+    // 如果 new_size 为 0，那么就直接 free
+    if(new_size == 0) {
+        free(ptr);
+        return NULL;
+    }
+
+    // 如果 new_size 大于 old_size，那么就直接 malloc
+    void *new_ptr = malloc(new_size);
+    if(!new_ptr) return NULL;
+
+    for(size_t i=0; i<new_size; i++)
+        *((char *)new_ptr + i) = *((char *)ptr + i);
+
+    free(ptr);
+    return new_ptr;
+}
+
+int main(int argc, char **argv) {
+    /* first malloc space for 5 int */
+    /* 为 5 int 分配第一个 malloc 空间 */
+    int *array = malloc(5 * sizeof(int));
+    if(!array) return -1;
+
+    for(int i=0; i<5; i++) {
+        array[i] = i*10;
+        printf("before realloc, array[%d] = %d\n", i, array[i]);
+    }
+
+    /* expand the size to 10 int */
+    /* 将大小扩展为 10 int */
+    array = my_realloc(array, 5*sizeof(int), 10*sizeof(int));
+    if(!array) return -1;
+
+    for(int i=5; i<10; i++)
+        array[i] = i*10;
+
+    for(int i=0; i<10; i++)
+        printf("after realloc, array[%d] = %d\n", i, array[i]);
+
+    free(array);
+    return 0;
+}
