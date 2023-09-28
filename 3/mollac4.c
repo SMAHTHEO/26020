f4f266bc0d45e360da6bb35e2568c8b941ef16de
c76258yx <yongyan.xie@student.manchester.ac.uk>
2023-09-28 21:17:25 +0100

    a

---
+++ 3/mollac4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    // 创建内存
+    int *a = malloc(10 * sizeof(int));
+    if(!a) return -1;
+
+    // 填数
+    for(int i=0; i<10; i++) {
+        a[i] = i;
+        printf("%d ", a[i]);
+    }
+
+    free(a);
+
+    return 0;
+
+}
