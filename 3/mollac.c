d918687c2e05f44eebf4b585d437f148a54fb72a
c76258yx <yongyan.xie@student.manchester.ac.uk>
2023-09-28 13:46:48 +0100

    a

--- 3/mollac.c
+++ 3/mollac.c
@@ -6,9 +6,7 @@ int main(int argc, char **argv) {
     printf("Number of arguments: %d\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("Argument %d: %s\n", i, argv[i]);
+    }
     return 0;
 
-    int *a;
-    a = malloc(sizeof(int)* 10);
-    free(a);
 }
