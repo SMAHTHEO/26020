dbfff59e233c1a89c6f4d6e6b3ee25b1ba40b66e
Theo <114228371+SMAHTHEO@users.noreply.github.com>
2023-10-20 13:11:44 +0100

    Update a.v

--- 1/a.v
+++ 1/a.v
@@ -8,21 +8,22 @@ module Stump_ALU (
     output reg [3:0] flags_out
 );
 
-// 临时变量
 reg [16:0] temp_result; // 用于捕获17位结果以获取进位
 
 always @(operand_A, operand_B, func, c_in, csh) begin
     // 默认值
-    result = 16'b0;
-    flags_out = 4'b0;
+    result <= 16'b0;
+    flags_out <= 4'b0;
 
     case(func)
-        3'b000: // ADD
+        3'b000: begin // ADD
             temp_result = operand_A + operand_B;
-            result = temp_result[15:0];
-        3'b001: // ADC
+            result <= temp_result[15:0];
+        end
+        3'b001: begin // ADC
             temp_result = operand_A + operand_B + c_in;
-            result = temp_result[15:0];
+            result <= temp_result[15:0];
+        end
         // ... 其他操作
 
     endcase
@@ -39,12 +40,3 @@ always @(operand_A, operand_B, func, c_in, csh) begin
 end
 
 endmodule
-###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(47):             result = temp_result[15:0];
-# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(47): near "=": syntax error, unexpected '=', expecting ++ or --.
-###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(53):     endcase
-# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(53): near "endcase": syntax error, unexpected endcase.
-###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(72): endmodule
-# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(72): near "endmodule": syntax error, unexpected endmodule.
-# End time: 13:09:59 on Oct 20,2023, Elapsed time: 0:00:01
-# Errors: 3, Warnings: 0
-# /cadtools5/mgc/questasim_2022.4/linux_x86_64/vlog failed.
