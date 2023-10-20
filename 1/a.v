7601344090c02615fd346d0fd9de401031a46f27
Theo <114228371+SMAHTHEO@users.noreply.github.com>
2023-10-20 13:10:25 +0100

    Update a.v
    a

--- 1/a.v
+++ 1/a.v
@@ -39,3 +39,12 @@ always @(operand_A, operand_B, func, c_in, csh) begin
 end
 
 endmodule
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(47):             result = temp_result[15:0];
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(47): near "=": syntax error, unexpected '=', expecting ++ or --.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(53):     endcase
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(53): near "endcase": syntax error, unexpected endcase.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(72): endmodule
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_ALU.v(72): near "endmodule": syntax error, unexpected endmodule.
+# End time: 13:09:59 on Oct 20,2023, Elapsed time: 0:00:01
+# Errors: 3, Warnings: 0
+# /cadtools5/mgc/questasim_2022.4/linux_x86_64/vlog failed.
