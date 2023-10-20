3ba54a942f99e3b2dcb44dac328ffeb28cc03198
Theo <114228371+SMAHTHEO@users.noreply.github.com>
2023-10-20 14:00:24 +0100

    Update a.v

--- 1/a.v
+++ 1/a.v
@@ -71,11 +71,13 @@ always @(operand_A, operand_B, func, c_in, csh) begin
         end
     endcase
 end
-// Clock-driven always block to latch the flags
-always @(posedge clk) begin
-    flags_out <= {result[15], (result == 16'b0), 
-                  (operand_A[15] & operand_B[15] & ~result[15]) | (~operand_A[15] & ~operand_B[15] & result[15]), 
-                  temp_result[16]};
+// Flags Generation
+always @(result, operand_A, operand_B, temp_result, csh) begin
+    flags_out[3] = result[15]; // N
+    flags_out[2] = (result == 16'b0) ? 1'b1 : 1'b0; // Z
+    flags_out[1] = (operand_A[15] & operand_B[15] & ~result[15]) | 
+                  (~operand_A[15] & ~operand_B[15] & result[15]); // V
+    flags_out[0] = temp_result[16]; // C
 end
 
 
