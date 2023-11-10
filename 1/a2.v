aa34dc1ada4c48d030ca65a36ab8ed0bf481ccaa
Theo <114228371+SMAHTHEO@users.noreply.github.com>
2023-11-10 16:27:41 +0000

    Update a2.v

--- 1/a2.v
+++ 1/a2.v
@@ -1,70 +1,31 @@
-// Internal signal and bus declarations
-reg [15:0] instruction;       // A register to hold a copy of the instruction if needed
-reg [2:0]  opcode;            // A register to hold the opcode extracted from the instruction
-reg        branch_taken;      // A signal to indicate whether a branch is taken
-wire [15:0] alu_result;       // A wire to hold the ALU result temporarily
-wire        zero_flag;        // A wire that is high when ALU result is zero (for condition codes)
-wire        carry_flag;       // A wire that indicates a carry out (for condition codes)
-wire        sign_flag;        // A wire that indicates the sign of the ALU result (for condition codes)
-wire        overflow_flag;    // A wire that indicates if there was an overflow (for condition codes)
-
-
-// Decoding logic goes here
-always @(*) begin
-    // Default all signals to 0 to avoid latches
-    fetch = 0;
-    execute = 0;
-    memory = 0;
-    ext_op = 0;
-    reg_write = 0;
-    dest = 0;
-    srcA = 0;
-    srcB = 0;
-    shift_op = 0;
-    opB_mux_sel = 0;
-    alu_func = 0;
-    cc_en = 0;
-    mem_ren = 0;
-    mem_wen = 0;
-    
-    // Decode the current state and set control signals accordingly
-    case (state)
-        `FETCH: begin
-            fetch = 1;
-            // During fetch, the instruction is read from memory into the IR
-            // PC is incremented after fetching the instruction
-            reg_write = 1;  // Enable register write to update the PC
-            srcA = `PC;     // Source A is PC for ALU to add increment
-            alu_func = `ALU_INC; // ALU function is increment
-            dest = `PC;     // Destination is PC for the result
-        end
-        `EXECUTE: begin
-            execute = 1;
-            // Decode the instruction and set control signals based on opcode
-            // This is a simplified example. You will need to expand this
-            // logic based on the specific opcodes and control signal requirements
-            case (ir[15:12]) // Assuming the opcode is in the ir[15:12] bits
-                `OP_ADD: begin
-                    alu_func = `ALU_ADD;   // Set ALU to perform ADD
-                    srcA = ir[11:9];       // Decode source register A
-                    srcB = ir[8:6];        // Decode source register B
-                    dest = ir[2:0];        // Decode destination register
-                    reg_write = 1;         // Enable register write for result
-                end
-                // Add more cases here for other instructions
-                // ...
-            endcase
-        end
-        `MEMORY: begin
-            memory = 1;
-            // Handle memory operations
-            // Set memory read or write signals based on LD/ST instruction
-            // Additional logic for LD/ST instructions will be similar to above
-            // and will include setting up address calculation, mem_ren, mem_wen, etc.
-        end
-        default: begin
-            // Handle any other states or default behavior
-        end
-    endcase
-end
-
+# ** Note: Multiple occurrences of ini variable Show_source are found, only first occurence will be considered.
+# QuestaSim-64 vlog 2022.4 Compiler 2022.10 Oct 18 2022
+# Start time: 16:26:40 on Nov 10,2023
+# vlog -reportprogress 300 -O0 -lint -cover bcsxf /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v 
+# -- Compiling module Stump_control_decode
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(68):             srcA = `PC;     // Source A is PC for ALU to add increment
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(68): (vlog-2163) Macro `PC is undefined.
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(68): near ";": syntax error, unexpected ';'.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(69):             alu_func = `ALU_INC; // ALU function is increment
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(69): (vlog-2163) Macro `ALU_INC is undefined.
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(69): near ";": syntax error, unexpected ';'.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(70):             dest = `PC;     // Destination is PC for the result
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(70): (vlog-2163) Macro `PC is undefined.
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(70): near ";": syntax error, unexpected ';'.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(78):                 `OP_ADD: begin
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(78): (vlog-2163) Macro `OP_ADD is undefined.
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(78): near ":": syntax error, unexpected ':'.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(79):                     alu_func = `ALU_ADD;   // Set ALU to perform ADD
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(79): (vlog-2163) Macro `ALU_ADD is undefined.
+###### ** while parsing macro expansion: 'MEMORY' starting at /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(89)
+# ** at /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(89):         `MEMORY: begin
+# ** Error: (vlog-13069) ** while parsing macro expansion: 'MEMORY' starting at /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(89)
+# ** at /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(89): near "2": syntax error, unexpected INTEGER NUMBER.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(117):   {N,Z,V,C} = CC;			// Break condition code register into flags
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(117): near "{": syntax error, unexpected '{'.
+###### /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(119):      0 : Testbranch =   1;	// Always (true)
+# ** Error: (vlog-13069) /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(119): near "=": syntax error, unexpected '='.
+# ** Error: /home/c76258yx/Questa/COMP22111/src/Stump/Stump_control_decode.v(119): (vlog-13205) Syntax error found in the scope following 'Testbranch'. Is there a missing '::'?
+# End time: 16:26:40 on Nov 10,2023, Elapsed time: 0:00:00
+# Errors: 13, Warnings: 0
+# /cadtools5/mgc/questasim_2022.4/linux_x86_64/vlog failed.
