566d4555bf765a2059d34146d38bc0e92eb2dc52
Theo <114228371+SMAHTHEO@users.noreply.github.com>
2023-11-10 16:25:24 +0000

    Update a2.v

--- 1/a2.v
+++ 1/a2.v
@@ -7,3 +7,64 @@ wire        zero_flag;        // A wire that is high when ALU result is zero (fo
 wire        carry_flag;       // A wire that indicates a carry out (for condition codes)
 wire        sign_flag;        // A wire that indicates the sign of the ALU result (for condition codes)
 wire        overflow_flag;    // A wire that indicates if there was an overflow (for condition codes)
+
+
+// Decoding logic goes here
+always @(*) begin
+    // Default all signals to 0 to avoid latches
+    fetch = 0;
+    execute = 0;
+    memory = 0;
+    ext_op = 0;
+    reg_write = 0;
+    dest = 0;
+    srcA = 0;
+    srcB = 0;
+    shift_op = 0;
+    opB_mux_sel = 0;
+    alu_func = 0;
+    cc_en = 0;
+    mem_ren = 0;
+    mem_wen = 0;
+    
+    // Decode the current state and set control signals accordingly
+    case (state)
+        `FETCH: begin
+            fetch = 1;
+            // During fetch, the instruction is read from memory into the IR
+            // PC is incremented after fetching the instruction
+            reg_write = 1;  // Enable register write to update the PC
+            srcA = `PC;     // Source A is PC for ALU to add increment
+            alu_func = `ALU_INC; // ALU function is increment
+            dest = `PC;     // Destination is PC for the result
+        end
+        `EXECUTE: begin
+            execute = 1;
+            // Decode the instruction and set control signals based on opcode
+            // This is a simplified example. You will need to expand this
+            // logic based on the specific opcodes and control signal requirements
+            case (ir[15:12]) // Assuming the opcode is in the ir[15:12] bits
+                `OP_ADD: begin
+                    alu_func = `ALU_ADD;   // Set ALU to perform ADD
+                    srcA = ir[11:9];       // Decode source register A
+                    srcB = ir[8:6];        // Decode source register B
+                    dest = ir[2:0];        // Decode destination register
+                    reg_write = 1;         // Enable register write for result
+                end
+                // Add more cases here for other instructions
+                // ...
+            endcase
+        end
+        `MEMORY: begin
+            memory = 1;
+            // Handle memory operations
+            // Set memory read or write signals based on LD/ST instruction
+            // Additional logic for LD/ST instructions will be similar to above
+            // and will include setting up address calculation, mem_ren, mem_wen, etc.
+        end
+        default: begin
+            // Handle any other states or default behavior
+        end
+    endcase
+end
+
